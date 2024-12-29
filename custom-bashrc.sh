9b057d234264ee17d9d07da56e1de7225aa7c481
zipyinthenet <ruben.b.clase2@gmail.com>
2024-12-29 23:34:30 +0100

    refactor

--- custom-bashrc.sh
+++ custom-bashrc.sh
@@ -3,9 +3,5 @@
 #backup original bashrc
 cp ~/.bashrc ~/.bashrc.bak.orig
 
-#change histsize
-#sed -i '/^HISTSIZE=/c\HISTSIZE=50000' ~/.bashrc
-#change histfilesize
-#sed -i '/^HISTFILESIZE=/c\HISTFILESIZE=100000' ~/.bashrc
-
+#copy .bashrc file repo github to home directory
 cp ~/custom-bashrc/.bashrc ~/.bashrc
