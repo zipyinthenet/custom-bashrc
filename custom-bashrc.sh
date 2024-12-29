946582f333c26f5d74addaeabe23a7d1a08b30cd
zipyinthenet <ruben.b.clase2@gmail.com>
2024-12-30 00:20:52 +0100

    add git-prompt.sh

--- custom-bashrc.sh
+++ custom-bashrc.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+#dependencies
+sudo apt update && sudo apt upgrade && sudo apt list --upgradable && sudo apt autoremove && sudo apt autopurge && sudo apt autoclean && sudo apt install wget -y 
+
 #backup original bashrc
 cp ~/.bashrc ~/.bashrc.bak.orig
+
+#download git-prompt.sh
+wget -O ~/custom-bashrc/git-prompt.sh https://github.com/git/git/raw/refs/heads/master/contrib/completion/git-prompt.sh
+cp ~/custom-bashrc/git-prompt.sh ~/.git-prompt.sh
 
 #copy .bashrc file repo github to home directory
 cp ~/custom-bashrc/.bashrc ~/.bashrc
